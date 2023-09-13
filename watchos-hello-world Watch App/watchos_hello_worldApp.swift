b1ceffdfdecd12f90c8c6fe4807b645fe3bcf322
Masao Kitamura <masaok@gmail.com>
2023-09-12 20:06:49 -0700

    Initial Commit

---
+++ watchos-hello-world Watch App/watchos_hello_worldApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchos_hello_worldApp.swift
+//  watchos-hello-world Watch App
+//
+//  Created by keckadmin on 9/12/23.
+//
+
+import SwiftUI
+
+@main
+struct watchos_hello_world_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
