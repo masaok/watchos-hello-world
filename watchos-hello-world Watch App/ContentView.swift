d061a2cad9e7c086c4de8d48d3c1f4834d7c8220
Masao Kitamura <masaok@gmail.com>
2023-09-13 01:10:41 -0700

    Trying various ways of saving data to iCloud

--- watchos-hello-world Watch App/ContentView.swift
+++ watchos-hello-world Watch App/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import CloudKit
 import Combine
 
 struct Response: Codable {
@@ -31,7 +32,11 @@ struct ContentView: View {
             Text("Hello, world!")
             
             Button("Save Data") {
+                let currentDate = Date().description
                 saveToiCloud()
+                saveToCloudKit(content: "Hello, CloudKit!")
+                saveToCloudKit(content: currentDate)
+                
             }
             
             Text("Last saved: \(savedDate)")
@@ -48,6 +53,53 @@ struct ContentView: View {
             }
         }
         .padding()
+    }
+    
+    // Save to CloudKit example
+    func saveToCloudKit(content: String) {
+        // Reference to public database
+        let publicDatabase = CKContainer.default().publicCloudDatabase
+        
+        // Create a new record
+        let newRecord = CKRecord(recordType: "SampleData")
+        newRecord["content"] = content as CKRecordValue
+        
+        // Save the record to iCloud
+        publicDatabase.save(newRecord) { (record, error) in
+            if let error = error {
+                // Handle the error
+                print("Error saving to CloudKit: \(error.localizedDescription)")
+            } else {
+                print("Saved to CloudKit!")
+            }
+        }
+    }
+    
+    // Load from CloudKit example
+    func loadFromCloudKit(completion: @escaping ([CKRecord]?, Error?) -> Void) {
+        // Reference to the public database
+        let publicDatabase = CKContainer.default().publicCloudDatabase
+        
+        // Create a query to fetch all 'SampleData' records
+        let query = CKQuery(recordType: "SampleData", predicate: NSPredicate(value: true))
+        
+        // Perform the fetch with the new method
+        publicDatabase.fetch(withQuery: query, inZoneWith: nil, desiredKeys: nil, resultsLimit: CKQueryOperation.maximumResults) { result in
+            switch result {
+            case .success(let data):
+                let records = data.matchResults.compactMap { (recordID, recordResult) -> CKRecord? in
+                    switch recordResult {
+                    case .success(let record):
+                        return record
+                    case .failure(_):
+                        return nil
+                    }
+                }
+                completion(records, nil)
+            case .failure(let error):
+                completion(nil, error)
+            }
+        }
     }
     
     // Save to iCloud example
